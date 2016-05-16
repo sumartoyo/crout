9bbd0db8064c11658297f82a9cf43f2703a6674b
Dimas <dimas.gs@outlook.com>
2016-05-16 21:00:54 +0700

    bugfix regula falsi

--- regula-falsi.c
+++ regula-falsi.c
@@ -7,8 +7,8 @@
 
 float f(float x)
 {
-	return exp(x)-5*pow(x,2); // slide
-	// return ((x * pow(2.1-0.5*x, 1/2)) / pow((1-x)*(1.1-0.5*x), 1/2)) - 3.69; // 1.a
+	// return exp(x)-5*pow(x,2); // slide
+	return (pow(x, 2)*(2.1-0.5*x)/(pow(1-x, 2)*(1.1-0.5*x)))-13.616;
 	// return tan(x) - x + 1; // 1.b
 	// return 0.5*exp(x/3) - sin(x); // 1.c
 }
@@ -17,8 +17,8 @@ int main(int argc, char **argv)
 {
 	float x0,x1,x2;
 
-	x0 = 0; x1 = 1; // slide
-	// x0 = 0; x1 = 1; // 1.a
+	// x0 = 0; x1 = 1; // slide
+	x0 = -0.2; x1 = 1.2; // 1.a
 	// x0 = 0; x1 = 3*M_PI; // 1.b
 	// x0 = 0; x1 = 1; // 1.c
 
